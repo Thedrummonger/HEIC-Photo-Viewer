444bf8acc9956bda889fef49c56122f9f530f54f
thedrummonger <drummerxtmail@gmail.com>
2022-05-20 10:33:40 -0500

    Add project files.

---
+++ HEIC Photo Viewer/ImageDisplayForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace HEIC_Photo_Viewer
+{
+    partial class ImageDisplayForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageDisplayForm));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsJPGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.zoomOutToolStripMenuItem,
+            this.zoomInToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsJPGToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.DropDownOpening += new System.EventHandler(this.fileToolStripMenuItem_DropDownOpening);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsJPGToolStripMenuItem
+            // 
+            this.saveAsJPGToolStripMenuItem.Name = "saveAsJPGToolStripMenuItem";
+            this.saveAsJPGToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveAsJPGToolStripMenuItem.Text = "Save as";
+            this.saveAsJPGToolStripMenuItem.Click += new System.EventHandler(this.saveAsJPGToolStripMenuItem_Click);
+            // 
+            // zoomOutToolStripMenuItem
+            // 
+            this.zoomOutToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("zoomOutToolStripMenuItem.Image")));
+            this.zoomOutToolStripMenuItem.Name = "zoomOutToolStripMenuItem";
+            this.zoomOutToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+            this.zoomOutToolStripMenuItem.Click += new System.EventHandler(this.zoomToolStripMenuItem_Click);
+            // 
+            // zoomInToolStripMenuItem
+            // 
+            this.zoomInToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("zoomInToolStripMenuItem.Image")));
+            this.zoomInToolStripMenuItem.Name = "zoomInToolStripMenuItem";
+            this.zoomInToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+            this.zoomInToolStripMenuItem.Click += new System.EventHandler(this.zoomToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 422);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // ImageDisplayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ImageDisplayForm";
+            this.Text = "HEIC Image View";
+            this.Load += new System.EventHandler(this.ImageDisplayForm_Load);
+            this.ResizeEnd += new System.EventHandler(this.ImageDisplayForm_ResizeEnd);
+            this.Resize += new System.EventHandler(this.ImageDisplayForm_ResizeEnd);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsJPGToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem zoomOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomInToolStripMenuItem;
+    }
+}
---
+++ HEIC Photo Viewer/ImageDisplayForm.cs
@@ -0,0 +1,178 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HEIC_Photo_Viewer
+{
+    public partial class ImageDisplayForm : Form
+    {
+        public ImageDisplayForm()
+        {
+            InitializeComponent();
+        }
+
+        private Image CurrentImage = null;
+        private string ImageName = null;
+        private int zoomLevel = 100;
+        private Point _OldMousePos = new Point(0, 0);
+
+        private Image ConvertHEICtoImage(string Path)
+        {
+            try
+            {
+                using var image = new MagickImage(Path);
+                image.Format = MagickFormat.Jpeg;
+                byte[] data = image.ToByteArray();
+                using var ms = new MemoryStream(data);
+                image.Dispose();
+                return Image.FromStream(ms);
+            }
+            catch { return null; }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+            if (result != DialogResult.OK) { return; }
+            OpenImageFromPath(openFileDialog.FileName);
+        }
+
+        private void OpenImageFromPath(string path)
+        {
+            Image ConvertedImage = ConvertHEICtoImage(path);
+            if (ConvertedImage is null) { return; }
+            pictureBox1.Image = ConvertedImage;
+            CurrentImage = ConvertedImage;
+            ImageName = Path.GetFileNameWithoutExtension(path);
+
+            this.Text = $"HEIC Image View: {ImageName}";
+
+            SizeObjects();
+            zoomInToolStripMenuItem.Visible = true;
+            zoomOutToolStripMenuItem.Visible = true;
+        }
+
+        private void saveAsJPGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (CurrentImage is null) { return; }
+            SaveFileDialog openFileDialog = new SaveFileDialog();
+            openFileDialog.FileName = $"{ImageName}";
+            openFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png|Tiff Image|*.tiff|Wmf Image|*.wmf";
+            openFileDialog.Title = "Saving Image";
+            var result = openFileDialog.ShowDialog();
+            if (result != DialogResult.OK) { return; }
+            saveImage(openFileDialog.FileName);
+        }
+
+        public void saveImage(string fileName)
+        {
+            Bitmap bmp = new Bitmap((Bitmap)CurrentImage.Clone());
+            string FileExtention = Path.GetExtension(fileName)??"";
+            switch (FileExtention)
+            {
+                case ".jpg":
+                    bmp.Save(fileName, ImageFormat.Jpeg);
+                    break;
+                case ".png":
+                    bmp.Save(fileName, ImageFormat.Png);
+                    break;
+                case ".bmp":
+                    bmp.Save(fileName, ImageFormat.Bmp);
+                    break;
+                case ".gif":
+                    bmp.Save(fileName, ImageFormat.Gif);
+                    break;
+                case ".tiff":
+                    bmp.Save(fileName, ImageFormat.Tiff);
+                    break;
+                case ".wmf":
+                    bmp.Save(fileName, ImageFormat.Wmf);
+                    break;
+                default:
+                    MessageBox.Show($"Failed to Save File!\n{FileExtention} format not recognized", "Failed to Save File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        private void ImageDisplayForm_ResizeEnd(object sender, EventArgs e)
+        {
+            SizeObjects();
+        }
+
+        private void SizeObjects()
+        {
+            if (pictureBox1.Image is null) { return; }
+            var currentScroll = panel1.AutoScrollPosition;
+            panel1.AutoScrollPosition = new Point(0, 0);
+            panel1.Width = this.Width - 16;
+            panel1.Height = this.Height - menuStrip1.Height - 42;
+            int PBX = (pictureBox1.Parent.ClientSize.Width / 2) - (pictureBox1.Image.Width / 2);
+            int PBY = (pictureBox1.Parent.ClientSize.Height / 2) - (pictureBox1.Image.Height / 2);
+            pictureBox1.Location = new Point(PBX < 0 ? 0 : PBX, PBY < 0 ? 0 : PBY);
+            panel1.AutoScrollPosition = new Point(Math.Abs(currentScroll.X), Math.Abs(currentScroll.Y));
+            pictureBox1.Refresh();
+        }
+
+        private void ImageDisplayForm_Load(object sender, EventArgs e)
+        {
+            zoomInToolStripMenuItem.Visible = false;
+            zoomOutToolStripMenuItem.Visible = false;
+            if (Environment.GetCommandLineArgs().Length > 1) { OpenImageFromPath(Environment.GetCommandLineArgs()[1]); }
+        }
+
+        private void fileToolStripMenuItem_DropDownOpening(object sender, EventArgs e)
+        {
+            saveAsJPGToolStripMenuItem.Visible = CurrentImage != null;
+        }
+
+        private void zoomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int modifier = 10;
+            if ((ModifierKeys & Keys.Control) == Keys.Control) { modifier = 1; }
+            if ((ModifierKeys & Keys.Shift) == Keys.Shift) { modifier = 20; }
+            zoomLevel = (sender == zoomOutToolStripMenuItem) ? zoomLevel - modifier : zoomLevel + modifier;
+            if (zoomLevel < 10) { zoomLevel = 10; }
+            DisplayResizedImage();
+            SizeObjects();
+        }
+
+        private void DisplayResizedImage()
+        {
+            int widthZoom = (CurrentImage.Width * zoomLevel) / 100;
+            int heightZoom = (CurrentImage.Height * zoomLevel) / 100;
+            pictureBox1.Image = resizeImage(CurrentImage, new Size(widthZoom, heightZoom));
+        }
+
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_OldMousePos == e.Location) { return; }
+            Point MouseLocation = Cursor.Position;
+            if (e.Button == MouseButtons.Left)
+            {
+                int CurrentHorizontalScroll = panel1.HorizontalScroll.Value;
+                int CurrentVErticalScroll = panel1.VerticalScroll.Value;
+                int HorizontalScrollAmmount = _OldMousePos.X - MouseLocation.X;
+                int VerticalScrollAmmount = _OldMousePos.Y - MouseLocation.Y;
+                panel1.AutoScrollPosition = new Point(CurrentHorizontalScroll + HorizontalScrollAmmount, CurrentVErticalScroll + VerticalScrollAmmount);
+            }
+            _OldMousePos = MouseLocation;
+        }
+    }
+}
